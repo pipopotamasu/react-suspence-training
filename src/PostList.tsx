ad5b5082d7509b8ac7b219104985ffa310d1511a
pipopotamasu <ec0156hx@gmail.com>
2022-01-02 18:07:43 +0900

    fetch posts

--- src/PostList.tsx
+++ src/PostList.tsx
@@ -1,10 +1,23 @@
+import { useQuery } from 'react-query';
+
+type Post = {
+  id: number;
+  title: string;
+  likes: number;
+};
+
+function getPosts(): Promise<Post[]> {
+  return fetch('http://localhost:3001/api/posts').then((res) => res.json());
+}
+
 export default function PostList() {
+  const { data } = useQuery('posts', getPosts);
+
   return (
-    <>
-      <main>
-        <h2>Who are we?</h2>
-        <p>That feels like an existential question, don&apos;t you think?</p>
-      </main>
-    </>
+    <ul>
+      {data?.map((post) => (
+        <li key={post.id}>{post.title}</li>
+      ))}
+    </ul>
   );
 }
