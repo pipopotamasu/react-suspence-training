f64ae2b3cd3f465d796d6650db7f730139f8e036
pipopotamasu <ec0156hx@gmail.com>
2022-01-04 17:53:11 +0900

    eslint

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,16 @@
 import React from 'react';
 // @ts-ignore
 import { createRoot } from 'react-dom';
-import { QueryClient, QueryClientProvider } from 'react-query';
 import 'tailwindcss/tailwind.css';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-const queryClient = new QueryClient();
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </QueryClientProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
